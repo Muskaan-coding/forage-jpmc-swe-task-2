ff0d51c3e3b932aeb27ba88170158baa9f018e20
Muskaan-coding <muskaanmarwa13@gmail.com>
2024-01-07 21:07:09 +0530

    task2

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  showGraph:boolean,
 }
 
 /**
@@ -22,6 +23,7 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      showGraph:true,
     };
   }
 
@@ -29,18 +31,38 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
+    if(this.state.showGraph){
     return (<Graph data={this.state.data}/>)
+    }
   }
 
   /**
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
+
+    let x=0;
+    const interval=setInterval(()=>{
       // Update the state by creating a new array of data that consists of
       // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+  console.log('Server Response:', serverResponds);
+
+  // Now try to parse the JSON
+  try {
+    this.setState({
+      data: serverResponds,
+      showGraph: true,
     });
+  } catch (error) {
+    console.error('Error parsing JSON:', error);
+    // Handle the error or non-JSON response here
+  }
+});
+
+    x++;
+    if(x>1000){
+    clearInterval(interval);}},100);
   }
 
   /**
